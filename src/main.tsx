8b9778c0eb9d89a910c6ef36f59a14368a446835
IsraelDCastro <fulanito203@gmail.com>
2023-06-13 09:19:58 -0400

    Some errors fixed on mobile

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import customTheme from "@/theme/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {/* <NextUIProvider theme={customTheme}> */}
-    <App />
-    {/* </NextUIProvider> */}
+    <NextUIProvider theme={customTheme}>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>
 );
